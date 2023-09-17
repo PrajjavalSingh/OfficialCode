e63e2b59dc79a8f968ae9dedbee05fee82fa8d63
Prajjaval Singh <prajjavalsingh@gmail.com>
2023-09-17 19:50:36 +0530

    code cleanup

--- Test_Image_Comparison.py
+++ Test_Image_Comparison.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[201]:
+# In[357]:
 
 
 import skimage 
@@ -11,13 +11,14 @@ from skimage import data
 from pylab import *
 from skimage import draw
 from skimage import exposure
+from skimage.filters import threshold_local
 from skimage import transform as tr
 from skimage import measure
 
 import cv2
 
 
-# In[223]:
+# In[358]:
 
 
 org_seis = cv2.imread('D:\\OfficeWork\\imagesforscript\\OrgPorject_Seis.jpg')
@@ -26,7 +27,7 @@ imp_seis = cv2.imread('D:\\OfficeWork\\imagesforscript\\Imported_Seis.jpg')
 #imp_seis = tr.resize( imp_seis, (org_seis.shape[0],org_seis.shape[1]) )
 
 
-# In[224]:
+# In[359]:
 
 
 print("Original Imported image size : ", imp_seis.shape)
@@ -36,7 +37,7 @@ print(type(imp_seis_resize.shape[1]))
 print(type(org_seis.shape[1]))
 
 
-# In[233]:
+# In[360]:
 
 
 #FUNCTIONS
@@ -52,11 +53,11 @@ def image_reSize( image, ratio ):
     length = int( image.shape[1] * ratio )
     width = int( image.shape[0] * ratio )
     dim = ( length, width )
-    return cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+    return cv2.resize(image, dim, interpolation = cv2.INTER_NEAREST )
 
-def plot_gray(image):
+def plot( image ):
     plt.figure(figsize=(16,10))
-    return plt.imshow(image, cmap='Greys_r')
+    return plt.imshow( image )
 
 def bw_scanner(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -87,39 +88,42 @@ def seismicCropImage( image ):
     seismic_window_contour = sorted(contours, key = cv2.contourArea, reverse = True)[:1]
     image_with_seismic_window_contour = cv2.drawContours(seis.copy(), seismic_window_contour, -1, (0,255,0), 1)
     #print("Image with Seismic Window Contour")
-    #plot_gray(image_with_seismic_window_contour)
+    #plot(image_with_seismic_window_contour)
     
     for c in seismic_window_contour:
         x,y,w,h = cv2.boundingRect(c)
-        actual_seismic_image = seis_gray[y:y+h, x:x+w]
+        actual_seismic_image = seis[y:y+h, x:x+w]
         break
     
     #cv2.imshow('Actual Seismic Image',actual_seismic_image)
-    #plot_gray(actual_seismic_image)
+    #plot(actual_seismic_image)
     
     return actual_seismic_image
 
 
-# In[234]:
+# In[361]:
 
 
 org_seis_sec = seismicCropImage(org_seis)
-plot_gray(org_seis_sec)
+plot(org_seis_sec)
 
 
-# In[235]:
+# In[362]:
 
 
 imp_seis_sec = seismicCropImage(imp_seis)
-plot_gray(imp_seis_sec)
+plot(imp_seis_sec)
 
 
-# In[236]:
+# In[363]:
 
 
 #converting image to GrayScale
-#org_seis_gray = cv2.cvtColor( org_seis_sec, cv2.COLOR_BGR2GRAY )
-#imp_seis_gray = cv2.cvtColor( imp_seis_sec, cv2.COLOR_BGR2GRAY )
+org_seis_sec = np.float32(org_seis_sec)
+imp_seis_sec = np.float32(imp_seis_sec)
+
+org_seis_sec = cv2.cvtColor( org_seis_sec, cv2.COLOR_BGR2GRAY )
+imp_seis_sec = cv2.cvtColor( imp_seis_sec, cv2.COLOR_BGR2GRAY )
 
 #Resize Images
 print("Before : Original Seismic Shape")
@@ -137,13 +141,19 @@ print("After : Imported Seismic Shape")
 print(imp_seis_sec.shape)
 
 print("Plotting Original Seismic after processing")
-plot_gray(org_seis_sec)
+plot(org_seis_sec)
 
 print("Plotting Imported Seismic section after processing")
-plot_gray(imp_seis_sec)
+plot(imp_seis_sec)
 
-mse_comp = mse(org_seis_sec,imp_seis_sec)
-print("MSE : " + str(mse_comp) )
+mse_comp = mse( org_seis_sec, imp_seis_sec )
+print( "MSE : " + str(mse_comp) )
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
